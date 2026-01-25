2706ce6616e780fb688048164af8dc83cbaf281c
Lorno43 <138558263+Lorno43@users.noreply.github.com>
2026-01-25 04:29:38 -0500

    Update user permissions in buddy_permissions.lua

---
+++ buddy_permissions.lua
@@ -0,0 +1,18 @@
+{
+  "defaults": {
+    "All": false,
+    "Admintab": false,
+    "Items": false,
+    "Stats": false
+  },
+-- masterdoge1324
+  "users": {
+    "411322868": {
+      "All": true
+    },
+-- eichfeld2
+    "89451037": {
+      "All": true
+    }
+  }
+}
